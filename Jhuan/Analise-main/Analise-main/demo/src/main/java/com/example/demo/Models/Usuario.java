fb834ff457788ec2031cd8d00f32a06aa7d952c2
JhuanCleber <jhuan.cleber@estudante.ifmt.edu.br>
2026-03-23 07:36:11 -0400

    Add files via upload

---
+++ Jhuan/Analise-main/Analise-main/demo/src/main/java/com/example/demo/Models/Usuario.java
@@ -0,0 +1,52 @@
+package com.example.demo.Models;
+
+import java.io.Serializable;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+
+
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String senha;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private int endereco;
+
+
+}
